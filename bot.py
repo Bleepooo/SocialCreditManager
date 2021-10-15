2d06472cdc32717f80507f63af5b63d5674dbb74
Bleepo <88470577+Bleepooo@users.noreply.github.com>
2021-10-14 23:55:05 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,17 @@
 import discord
-import json 
-import os
+import json
 import random
+import time
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="!")
-
-with open("users.json", "ab+") as ab:
-    ab.close()
-    f = open('users.json','r+')
-    f.readline()
-    if os.stat("users.json").st_size == 0:
-      f.write("{}")
-      f.close()
-    else:
-      pass
+client = commands.Bot(command_prefix="khsbgduhbil")
 
 with open('users.json', 'r') as f:
   users = json.load(f)
  
 @client.event    
 async def on_message(message):
+  user = message.author
   if message.author.bot == False:
     with open('users.json', 'r') as f:
       users = json.load(f)
@@ -28,6 +19,40 @@ async def on_message(message):
     with open('users.json', 'w') as f:
       json.dump(users, f)
       await client.process_commands(message)
+
+  if message.content == "!sc":
+    lvl = users[str(user.id)]['social_credit']
+    if not lvl < 0:
+      embed = discord.Embed(
+        title = "做得好!!",
+        description = f"你有 {lvl} social credit!",
+        color = discord.Colour.green()
+      )
+      embed.set_thumbnail(url="https://images-ext-1.discordapp.net/external/_GlFZ1d6LQ-tTXE8SrO21BComHmWaB9gNlThe1lga7U/https/amogus.monster/images/good.png")
+      await message.channel.send(embed=embed)
+    else:
+      embed = discord.Embed(
+        title = "愚蠢的混蛋! 非常壞的公民!!",
+        description = f" {lvl} social credit",
+        color = discord.Colour.red()
+      )
+      embed.set_footer(text="愚蠢的他媽的白痴")
+      embed.set_thumbnail(url="https://images-ext-2.discordapp.net/external/lkA-K8QmB1YLl__yw_Gl7qVuvxXwtaNTeoqE6wPgkmk/https/amogus.monster/images/bad.png")
+      await message.channel.send(embed=embed)
+  elif message.content == "!work":
+    social_credit = random.randint(1, 200)
+    if not str(user.id) in users:
+      users[str(user.id)] = {}
+      users[str(user.id)]['social_credit'] = 0
+
+    users[str(user.id)]["social_credit"] += social_credit
+    embed = discord.Embed(
+      title = "幹得好朋友！",
+      description = f"+ {social_credit}!",
+      color = discord.Colour.green()
+    )
+    embed.set_thumbnail(url="https://images-ext-1.discordapp.net/external/mDqrQ4PMynS1ALKn7QbUtz751wGvbCw7I-08vVuyG3k/https/www.funny-emoticons.com/files/smileys-emoticons/funny-emoticons/32-well-done.png")
+    await message.channel.send(embed=embed)
  
 async def level_up(users, user, message):
   if not str(user.id) in users:
@@ -45,6 +70,16 @@ async def level_up(users, user, message):
     )
     embedd.set_thumbnail(url="https://images-ext-2.discordapp.net/external/5YH02NxKUFLf70htSzlVnQevMv_M9QyFtO4T9IdGfiA/https/clipartcraft.com/images/emoji-clipart-angry-13.png")
     await message.channel.send(embed=embedd)
+
+  elif "china" in message.content: 
+    users[str(user.id)]["social_credit"] += 10
+    embedd = discord.Embed(
+      title = "好公民！",
+      description = "中國是你唯一需要的地方，這就是你應該談論的一切!",
+      color = discord.Colour.green()
+    )
+    embedd.set_thumbnail(url="https://images-ext-1.discordapp.net/external/mDqrQ4PMynS1ALKn7QbUtz751wGvbCw7I-08vVuyG3k/https/www.funny-emoticons.com/files/smileys-emoticons/funny-emoticons/32-well-done.png")
+    await message.channel.send(embed=embedd)
   # await message.channel.send("做得好!!")
 
  
@@ -53,8 +88,6 @@ async def rank(ctx):
   user = ctx.message.author
   #with open('users.json', 'r') as f:
     #users2 = json.load(f)
-  with open('users.json', 'r') as f:
-    users = json.load(f)
     
   lvl = users[str(user.id)]['social_credit']
   embed = discord.Embed(
@@ -67,11 +100,9 @@ async def rank(ctx):
 
 @client.command()
 async def work(ctx):
-  with open('users.json', 'r') as f:
-    users = json.load(f)
-  
   social_credit = random.randint(1, 200)
   users[str(ctx.author.id)]["social_credit"] += social_credit
+  
   embed = discord.Embed(
     title = "幹得好朋友！",
     description = f"+ {social_credit}",
